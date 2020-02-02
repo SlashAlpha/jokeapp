6205f3edff8ad73ffe2e26f048d030054dde1352
slashuzero <duprat.c@live.com>
2020-02-02 18:03:10 +0100

    Creating Joke App

---
+++ src/main/java/slash/process/fun/jokeapp/Services/JokeService.java
@@ -0,0 +1,5 @@
+package slash.process.fun.jokeapp.Services;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/main/java/slash/process/fun/jokeapp/controller/JokeController.java
@@ -0,0 +1,23 @@
+package slash.process.fun.jokeapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import slash.process.fun.jokeapp.Services.JokeService;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("jokes", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
